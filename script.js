74d7c65a31bcd4feb8445ec589b082fc90b6aa38
Hizude <k.serkanertas@gmail.com>
2020-07-25 21:39:09 +0200

    added all button/display functionality. todo: keyboard functionality, improve readability of code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// On numBtn press, append num to currentOperand.innerText
-// have 3 vars: prev, operator, current
- // on operator press, move operand up and type new num in current operand
+// TODO: keyboard functionality, improve code readability, input for 
+// amount of digits after comma
 
+let allBtns = document.querySelectorAll('button');
 const numberBtns = document.querySelectorAll('[data-number]');
 const operationBtns = document.querySelectorAll('[data-operation]');
 const equalBtn = document.querySelector('[data-equals]');
@@ -9,29 +9,63 @@ const ansBtn = document.querySelector('[data-ans]');
 const clearEverythingBtn = document.querySelector('[data-clear-everything]');
 const clearBtn = document.querySelector('[data-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
+const outputDiv = document.querySelector('.output');
 let currentOperand = document.querySelector('[data-current-operand]');
 let previousOperand = document.querySelector('[data-previous-operand]');
-let prev;
 let operator;
-let current;
-let computation;
+let ans = 0;
 
+// functions
+// add the number/operator pressed to the section below.
 function append(number) {
     if (number === '.' && currentOperand.innerText.includes('.')) return;
-    if (currentOperand.innerText === '' && isNaN(number)) return;
     currentOperand.innerText += number;
 }
 
+// if the section above is not empty, compute. Then, add the operator to the section below.
 function chooseOperator(chosenOperator) {
+    if (currentOperand.innerText === '') return;
+    if (previousOperand.innerText !== '') {
+        compute()
+    }
     operator = chosenOperator;
     append(operator);
-    if (currentOperand == '' && number == operator) {
-        updateDisplay();
-    }
+    updateDisplay();
 }
 
+// after that, the top section gets the below section appened to it. the below section gets cleared. 
 function updateDisplay() {
     previousOperand.innerText += currentOperand.innerText;
+    currentOperand.innerText = '';
+}
+
+function compute() {
+    let computation;
+    let prev = parseFloat(previousOperand.innerText);
+    let current = parseFloat(currentOperand.innerText);
+    if (isNaN(prev) || isNaN(current)) return;
+    switch(operator){
+        case '+':
+            computation = prev + current;
+            break;
+
+        case '-':
+            computation = prev - current;
+            break;
+            
+        case '÷':
+            computation = prev / current;
+            break;
+
+        case '*':
+            computation = prev * current;
+            break;
+
+        default:
+            return;
+    }
+    previousOperand.innerText = parseFloat(computation.toFixed(4));
+    ans = previousOperand.innerText;
     currentOperand.innerText = '';
 }
 
@@ -45,14 +79,38 @@ function clearEverything() {
 }
 
 function clear() {
-    //
+    currentOperand.innerText = '';
 }
 
 // 'delete' is reserved word so 'backspace' is alternative.
 function backspace() {
-    //
+    currentOperand.innerText = currentOperand.innerText.slice(0,-1);
 }
 
+function disableInput() {
+    allBtns.forEach(button => {
+        button.disabled = true;
+    })
+    clearEverythingBtn.disabled = false;
+}
+
+function enableInput() {
+    allBtns.forEach(button => {
+        button.disabled = false;
+    })
+}
+
+function newRound() {
+    disableInput();
+    clearEverythingBtn.classList.add('new-round-btn');
+    outputDiv.classList.add('new-round-output');
+}
+
+function ansFunc() {
+    append(ans)
+}
+
+// event listeners
 numberBtns.forEach(button => {
     button.addEventListener('click', () => {
         append(button.innerText);
@@ -61,26 +119,31 @@ numberBtns.forEach(button => {
 operationBtns.forEach(button => {
     button.addEventListener('click', () => {
         chooseOperator(button.innerText);
-        updateDisplay();
+        // updateDisplay();
     })
 })
 
 equalBtn.addEventListener('click', () => {
+    if (previousOperand.innerText === '' || currentOperand.innerText === '') return;
     compute();
+    newRound();
 })
 
 ansBtn.addEventListener('click', () => {
-    //
+    ansFunc();
 })
 
 clearEverythingBtn.addEventListener('click', () => {
     clearEverything();
+    clearEverythingBtn.classList.remove('new-round-btn');
+    outputDiv.classList.remove('new-round-output');
+    enableInput();
 })
 
 clearBtn.addEventListener('click', () => {
-    //
+    clear();
 })
 
 deleteBtn.addEventListener('click', () => {
-    //
+    backspace();
 })
